38dfa07c77c3341fbcc417aa8ae0fcfa050456b2
george.gusakov <george.gusakov@ostrovok.ru>
2025-12-23 10:49:01 +0300

    Fix parsing year form url

--- src/logic/getYear.ts
+++ src/logic/getYear.ts
@@ -1,6 +1,6 @@
 export const getYear = (): number => {
   const currentYear = new Date().getFullYear();
-  const yearMatch = window.location.pathname.match(/\/\d+$/)?.[0];
+  const yearMatch = window.location.search.match(/^\?\d+$/)?.[0];
 
   return yearMatch ? Number(yearMatch.slice(1)) : currentYear;
 }
